cb247ebc58a9921779b48313cbefdd59219d0eb8
yagodin <dmitry.yagodin@gmail.com>
2024-03-29 07:45:30 +0200

    feat: add hedgehog model and init

--- server/db_migrations/0002_hedgehog_table.sql
+++ server/db_migrations/0002_hedgehog_table.sql
@@ -1,1 +1,36 @@
-CREATE TABLE hedgehog ("id" serial NOT NULL);
+CREATE TYPE GENDER_ENUM AS ENUM ('male', 'female', 'unknown');
+
+CREATE TABLE hedgehog (
+	"id" serial PRIMARY KEY,
+	"name" VARCHAR(100),
+	"age" INTEGER,
+	"gender" GENDER_ENUM,
+	"coordinates" GEOMETRY(Point, 3857),
+	"date" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO
+	hedgehog ("name", "age", "gender", "coordinates")
+VALUES
+	(
+		'Sonic',
+		3,
+		'male',
+		ST_SetSRID(
+			ST_MakePoint(2643005.6750608203, 8743508.703183644),
+			3857
+		)
+	);
+
+INSERT INTO
+	hedgehog ("name", "age", "gender", "coordinates")
+VALUES
+	(
+		'Sonic2',
+		50,
+		'female',
+		ST_SetSRID(
+			ST_MakePoint(3085311.6450856356, 10906097.626616776),
+			3857
+		)
+	);
--- shared/src/hedgehog.ts
+++ shared/src/hedgehog.ts
@@ -1,11 +1,25 @@
 import { z } from "zod";
 
+
+const messages = {
+  age: 'Oletko varmaa? Siili elää keskimäärin kolmen tai neljän vuoden ikäiseksi. Maailman vanhin tieteellisesti vahvistettu eurooppalainen siili on löydetty Tanskasta kansalaistieteellisessä hankkeessa, johon osallistui satoja vapaaehtoisia. Siili eli 16 vuotta, 7 vuotta pidempään kuin edellinen ennätyksen haltija.'
+}
+
 /**
  * Hedgehog interface shared between server and client
  */
 
 export const hedgehogSchema = z.object({
-  id: z.number(), // TODO: loput siilin tietomallista. Zod:lta löytyy esimerkiksi tällaisia tyyppejä: z.enum(), z.string(), z.number() jne. joita voi olla tarpeen hyödyntää
+  id: z.number().optional(),
+  name: z.string().min(1, { message: "Nimi kenttä on pakollinen." }).max(100, { message: "Nimi on liian pitkä." }),
+  age: z.number().min(1, { message: "Ikä kenttä on pakollinen" }).max(20, { message: messages.age }),
+  gender: z
+    .string()
+    .refine(value => value === "male" || value === "female" || value === "unknown", {
+      message: "Sukupuoli kenttä on pakollinen.",
+    }),
+  coordinates: z.array(z.number()).length(2),
+  date: z.number().optional()
 });
 
 export type Hedgehog = z.infer<typeof hedgehogSchema>;
