3e0f94c8c8ff0b872c48605de29e99b955b5c2aa
yagodin <dmitry.yagodin@gmail.com>
2024-03-29 07:48:44 +0200

    feat: add server api

--- server/src/application/hedgehog.ts
+++ server/src/application/hedgehog.ts
@@ -1,20 +1,88 @@
 import { getPool } from "@server/db";
 import { logger } from "@server/logging";
-import { hedgehogSchema } from "@shared/hedgehog";
+import { Hedgehog, hedgehogSchema } from "@shared/hedgehog";
 import { sql } from "slonik";
 
 export async function getAllHedgehogs() {
   try {
-    const hedgehogs = await getPool().any(
-      sql.type(hedgehogSchema)`SELECT id FROM hedgehog`
-    );
+    const query = sql.type(hedgehogSchema)`
+    SELECT
+      id, 
+      name, 
+      age,
+      gender, 
+      json_build_array(
+        ST_X(coordinates::geometry),
+        ST_Y(coordinates::geometry)
+      ) as coordinates,
+      date
+    FROM
+      hedgehog;   
+    `
 
-    return hedgehogs;
+    return await getPool().any(query);
   } catch (error) {
     logger.error(error);
   }
 }
 
-// TODO: Yksittäisen siilin hakeminen tietokannasta ID:llä
+/**
+ * Fetch a hedgehog by ID
+ */
+export async function getHedgehogById(id: number ) {
+  if (!id) throw new Error("ID is required");
+  try {
+    const query = sql.type(hedgehogSchema)`
+      SELECT
+        id, 
+        name, 
+        age,
+        gender, 
+        json_build_array(
+          ST_X(coordinates::geometry),
+          ST_Y(coordinates::geometry)
+        ) as coordinates,
+        date
+      FROM 
+        hedgehog
+      WHERE
+        id = ${id};
+    `;
 
-// TODO: Yksittäisen siilin lisäämisen sovelluslogiikka
+    const hedgehog: Hedgehog | null = await getPool().maybeOne(query);
+    if (!hedgehog) {
+      throw new Error("Hedgehog not found");
+    }
+    return hedgehog;
+  } catch (error) {
+    logger.error(error);
+  }
+}
+
+/**
+ * Add a new hedgehog to the database
+ */
+export async function insertHedgehog({name, age, gender, coordinates}: Hedgehog ) {
+  try {
+    const query = sql.type(hedgehogSchema)`
+      INSERT INTO
+        hedgehog (name, age, gender, coordinates)
+      VALUES (
+          ${name},
+          ${age},
+          ${gender},
+          ST_SetSRID(ST_MakePoint(${coordinates[0]}, ${coordinates[1]}), 3857)
+      )
+      RETURNING id;
+    `;
+
+    const { id } = await getPool().one(query);
+
+    if (!id) {
+      throw new Error('Failed to return an insert ID');
+    }
+    return id;
+  } catch (error) {
+    logger.error(error);
+  }
+}
--- server/src/routes/hedgehog.ts
+++ server/src/routes/hedgehog.ts
@@ -1,4 +1,5 @@
-import { getAllHedgehogs } from "@server/application/hedgehog";
+import { getAllHedgehogs, getHedgehogById, insertHedgehog } from "@server/application/hedgehog";
+import { hedgehogSchema } from '@ubigu/shared/src/hedgehog';
 import { FastifyInstance, FastifyPluginOptions } from "fastify";
 
 export function hedgehogRouter(
@@ -7,18 +8,48 @@ export function hedgehogRouter(
   done: () => void
 ) {
   fastify.get("/", async function (_request, reply) {
-    const hedgehogs = await getAllHedgehogs();
+    
+    try {
+      const hedgehogs = await getAllHedgehogs();
 
-    return reply.code(200).send({
-      hedgehogs,
-    });
+      if (hedgehogs && hedgehogs.length) {
+        const ids = hedgehogs.map(hedgehog => hedgehog.id);
+        return reply.code(200).send({ids});
+      }
+
+    } catch (error) {
+      console.error(error)
+      return reply.status(500).send({ success: false, error: "Internal Server Error" });
+    }
+
   });
 
-  // TODO: Yksittäisen siilin hakeminen tietokannasta ID:llä
-  // fastify.get(...);
+  fastify.put("/", async function (_request, reply) {
+    try {
+        const params = await _request.body;        
+        const id = await insertHedgehog(hedgehogSchema.parse(params));
+        return reply.code(200).send({ success: true, id });
 
-  // TODO: Yksittäisen siilin lisäämisen sovelluslogiikka
-  // fastify.post(...)
+    } catch (error) {
+      console.error(error);
+      return reply.status(500).send({ success: false, error: "Internal Server Error" });
+    }
+  });
+
+
+  fastify.get("/:id", async function (_request, reply) {
+    const param = await _request.params as {id: string};
+    try {
+      if (parseInt(param.id)) {
+        const hedgehog = await getHedgehogById(parseInt(param.id));
+        return reply.code(200).send({ success: true, hedgehog });
+      }
+      throw Error('Indvalid request')
+    } catch (error) {
+      console.error(error);
+      return reply.status(500).send({ success: false, error: "Internal Server Error"});
+    }
+  });
 
   done();
 }
